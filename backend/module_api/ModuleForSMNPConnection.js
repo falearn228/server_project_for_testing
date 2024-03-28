e837b2f1491d9b7f61cfe6264118cf907bf6ef0d
Uniciti <90056671+Uniciti@users.noreply.github.com>
2024-03-28 17:45:58 +0700

    Подключил фронт для snmp

--- backend/module_api/ModuleForSMNPConnection.js
+++ backend/module_api/ModuleForSMNPConnection.js
@@ -6,25 +6,27 @@ const options = {
   port: 161, // порт SNMP-агента на устройстве
 };
 
-const oid = '1.3.6.1.4.1.19707.7.7.2.1.4.18.0';
+//const oid = '1.3.6.1.4.1.19707.7.7.2.1.4.18.0';
 // Установка соединения с устройством
 const session = snmp.createSession(options.host);
 
-// Отправка запроса на чтение значения OID
-session.get([oid], function (error, varbinds) {
-  if (error) {
-    console.error('Ошибка при чтении OID:', error);
-  } else {
-    for (const varbind of varbinds) {
-      if (snmp.isVarbindError(varbind)) {
-        console.error('Ошибка в varbind:', varbind);
-      } else {
-        console.log('OID:', varbind.oid);
-        console.log('Значение:', varbind.value.toString());
+function snmpGet(oid){
+  session.get([oid], function (error, varbinds) {
+    if (error) {
+      console.error('Ошибка при чтении OID:', error);
+    } else {
+      for (const varbind of varbinds) {
+        if (snmp.isVarbindError(varbind)) {
+          console.error('Ошибка в varbind:', varbind);
+        } else {
+          console.log('OID:', varbind.oid);
+          console.log('Значение:', varbind.value.toString());
+        }
       }
     }
-  }
-});
+  });
+  
+}
 
 var MyVarbinds = [
   {
--- backend/src/micranServer.js
+++ backend/src/micranServer.js
@@ -13,7 +13,23 @@ app.use(bodyParser.json());
 const frontPath = path.join(__dirname + '../../../frontend');
 app.use(express.static('/fontend/public'));
 
-// Обработка запроса от клиента
+
+// начало SNMP модуль
+
+app.get("/snmp", (req, res) =>{
+  res.sendFile("/Telemetry.html", {root: path.join(frontPath, 'public')});
+});
+
+app.get("/styles.css", (req, res) =>{
+res.sendFile("/styles.css", {root: path.join(frontPath, 'styles')});
+});
+
+app.get("/script.js", (req, res) =>{
+res.sendFile("/script.js", {root: path.join(frontPath, 'src')});
+});
+// Конец SNMP модуля
+
+// Начало беркут модуль
 app.post('/bert/process', async (req, res) => {
       const command = req.body.command;
     try{
@@ -56,6 +72,7 @@ app.get("/stylesIMPROVED.css", (req, res) =>{
 app.get("/scriptIMPROVED.js", (req, res) =>{
   res.sendFile("/scriptIMPROVED.js", {root: path.join(frontPath, 'src')});
 });
+// Конец беркут модуля
 
 app.listen(port, () => {
      console.log(`Server is running on http://localhost:${port}`);
