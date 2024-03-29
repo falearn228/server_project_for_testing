b3cb76bfeb6aa04db78c1c9e43f4a7a940fd0cfb
CherryBoyGameMaker <deuscybbota@gmail.com>
2024-03-29 12:59:02 +0700

    Mode snmp module

--- backend/module_api/ModuleForSMNPConnection.js
+++ backend/module_api/ModuleForSMNPConnection.js
@@ -3,14 +3,66 @@ const snmp = require('net-snmp');
 // Настройки для подключения к устройству
 const options = {
   host: '172.16.17.220',
-  port: 161, // порт SNMP-агента на устройстве
 };
 
-//const oid = '1.3.6.1.4.1.19707.7.7.2.1.4.18.0';
-// Установка соединения с устройством
-const session = snmp.createSession(options.host);
+var MyVarbinds = [//example of structre
+  {
+      oid: "",
+      type: snmp.ObjectType.Integer32,
+      value: 0
+  }
+];
 
-function snmpGet(oid){
+function oidMaker(whatOid,Value)
+{
+  switch(whatOid)
+  {
+    case "Frequncy":
+    {
+      MyVarbinds.oid=""
+      MyVarbinds.value=Value
+      break
+    }
+    case "Width":
+    {
+      MyVarbinds.oid=""
+      MyVarbinds.value=Value
+      break
+    }
+    case "mode":
+    {
+      MyVarbinds.oid="1.3.6.1.4.1.19707.7.7.2.1.4.18.0"
+      MyVarbinds.value=Value
+      break
+    }
+  }
+}
+
+function setOid(oidToSet, value)
+{
+  oidMaker(oidToSet, value)
+  const session = snmp.createSession(options.host);
+
+  session.set (MyVarbinds, function (error, varbinds) {
+    if (error) {
+        console.error (error.toString ());
+    } else {
+        for (var i = 0; i < MyVarbinds.length; i++) {
+            if (snmp.isVarbindError (MyVarbinds[i]))
+                console.error (snmp.varbindError (MyVarbinds[i]));
+            else
+                console.log (MyVarbinds[i].oid + "|" + MyVarbinds[i].value);
+        }
+    }
+    
+    session.close();
+  });
+}
+
+function getOid(oid)
+{
+  const session = snmp.createSession(options.host);
+
   session.get([oid], function (error, varbinds) {
     if (error) {
       console.error('Ошибка при чтении OID:', error);
@@ -21,47 +73,16 @@ function snmpGet(oid){
         } else {
           console.log('OID:', varbind.oid);
           console.log('Значение:', varbind.value.toString());
+          session.close(); 
+          return arbind.value.toString();
         }
       }
     }
+    session.close();
+    return "";
   });
-  
 }
-
-var MyVarbinds = [
-  {
-      oid: "1.3.6.1.4.1.19707.7.7.2.1.4.18.0",
-      type: snmp.ObjectType.Integer32,
-      value: 1
-  }
-];
-
-session.set (MyVarbinds, function (error, varbinds) {//maybe dont work
-  if (error) {
-      console.error (error.toString ());
-  } else {
-      for (var i = 0; i < MyVarbinds.length; i++) {
-          if (snmp.isVarbindError (MyVarbinds[i]))
-              console.error (snmp.varbindError (MyVarbinds[i]));
-          else
-              console.log (MyVarbinds[i].oid + "|" + MyVarbinds[i].value);
-      }
-  }
-});
-
-session.get([oid], function (error, varbinds) {
-  if (error) {
-    console.error('Ошибка при чтении OID:', error);
-  } else {
-    for (const varbind of varbinds) {
-      if (snmp.isVarbindError(varbind)) {
-        console.error('Ошибка в varbind:', varbind);
-      } else {
-        console.log('OID:', varbind.oid);
-        console.log('Значение:', varbind.value.toString());
-      }
-    }
-  }
-  
-  session.close();
-});
+module.exports = {
+  setOid,
+  getOid
+};
